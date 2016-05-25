87f834d90e4e1c235c914e53f435afdd13c0031d
Elias Boroda <borod002@umn.edu>
2016-05-25 13:46:47 -0500

    Initial checkin

---
+++ src/TaskCore/Assets/Scripts/ChoiceLayout.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChoiceLayout : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        var width = Camera.main.orthographicSize;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        var width = Camera.main.orthographicSize * 2.0 * Screen.width / Screen.height;
+
+        //transform.localScale = new Vector3((float)width * 0.9f, (float)width * 0.9f, (float)width * 0.9f);
+    }
+}
---
+++ src/TaskCore/Assets/Scripts/Difficulty.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class Difficulty
+{
+    private int _lowestLevel = 1;
+    private int _currentLevel = 1;
+
+    private int _numberCorrectAtThisLevel = 0;
+    private int _numberIncorrectAtThisLevel = 0;
+    private int _totalCorrect = 0;
+    private int _totalIncorrect = 0;
+
+    //a public property that takes as input the startinglevel from taskengine
+    public Difficulty(int startingLevel)
+    {
+        _currentLevel = startingLevel;
+    }
+
+    //returns an int (the _currentLevel) and takes input from AttemptEval as to whether the previous answer was correct or not
+    public int ProcessAttempt(AttemptEval result)
+    {
+        if (result == AttemptEval.Incorrect)
+        {
+            _numberIncorrectAtThisLevel++;
+            _totalIncorrect++;
+        }
+        else
+        {
+            _numberCorrectAtThisLevel++;
+            _totalCorrect++;
+        }
+
+        if (_numberIncorrectAtThisLevel + _numberCorrectAtThisLevel >= 5)
+        {
+            float performance = ((float)_numberCorrectAtThisLevel / (float)(_numberCorrectAtThisLevel + _numberIncorrectAtThisLevel));
+            if (performance < 0.7)
+            {
+                if (_currentLevel != _lowestLevel)
+                    _currentLevel--;
+                
+                _numberCorrectAtThisLevel = 0;
+                _numberIncorrectAtThisLevel = 0;
+                
+            }
+            else
+            {
+                if (performance > 0.9)
+                {
+                    _currentLevel++;
+                    _numberCorrectAtThisLevel = 0;
+                    _numberIncorrectAtThisLevel = 0;
+                }
+            }
+        }
+
+        return _currentLevel;
+    }
+}
+
+public enum AttemptEval
+{
+    Init,
+    Correct,
+    Incorrect
+}
+
